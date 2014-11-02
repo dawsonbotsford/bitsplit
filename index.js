16791cb9d3ba9d3cb90b6468954f1cf3aa574ecb
Peyman Mortazavi <pmortazavi@cailmobility.com>
2014-11-02 01:12:32 -0700

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 //
-// app.get('/', function(request, response) {
-//   response.send(cool());
-// });
 
 app.use(express.static(__dirname + '/auth'))
+
+app.use(bodyParser.json())
+
+app.post('/submit', function(request, response) {
+
+  	response.send(request.body.firstName);
+  	
+});
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
