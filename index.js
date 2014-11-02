a1bbbe23c056ebe9ae27a76b55097f2abdd0aa81
Brian Newsom <brian.newsom@colorado.edu>
2014-11-02 01:17:23 -0800

    Fix index.js

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ var coreJs = require('./js/core.js');
 app.set('port', (process.env.PORT || 5000))
 
 
-app.use(express.static(__dirname + '/auth'))
+app.use(express.static(__dirname + '/'))
+
+//app.use(express.static(__dirname + '/css'))
 
 app.use(bodyParser.json())
 
