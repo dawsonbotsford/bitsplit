ca4062e2f89d077011f64cd2eddd2a7697ecc4bf
Peyman Mortazavi <pmortazavi@cailmobility.com>
2014-11-02 04:41:07 -0800

    a

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var coreJs = require('./js/core.js');
+var bitcoin = require('./bitcoin.js');
+var Parse = require('node-parse-api').Parse;
 
+var parse = new Parse("is3SIL9nDLCqIhhOp3S9v1f8K7PiXs9mTjRwDkPs", "av6qlnU6r8pl2Wt78wSVmmFoxiEvZwnGMNm97S0D");
 
 app.set('port', (process.env.PORT || 5000))
-
 
 app.use(express.static(__dirname + '/'))
 
@@ -13,18 +15,42 @@ app.use(express.static(__dirname + '/'))
 
 app.use(bodyParser.json())
 
-app.post('/submit', function(request, response) {
+app.post('/invoice', function(request, response) {
 
-  	response.send(request.body.firstName);
+	var keys = bitcoin.generateKeys();
+
+	var invoiceObject =
+	{ 
+			message: request.body.message,
+			invoice_privateKey: keys.private_key,
+			invoice_publicKey: keys.public_key,
+			receiver_publicKey: request.body.receiver_publicKey,
+			sender_publicKey: request.body.sender_publicKey,
+			amount: request.body.amount,
+			isPaid: false
+	};
+
+	parse.insert('Invoice', invoiceObject, 
+	 	function(err, res) {
+
+  			response.send(res);
+	})
 
 });
 
-app.post('/split', function(request, response) {
-    response.send(response.send(coreJs.calculate(request.body)));
+app.post('/paymentreceived', function(request, response) {
+
+
+	// get transaction
+	var inputs = request.body.payload.input_addresses;
+	var outputs = request.body.payload.output_addresses;
+
+	response.send("{'success':true}");
+
 })
 
-app.get('/test', function(request, response){
-	response.send("{'name':'PEYMAN'}");
+app.post('/split', function(request, response) {
+    response.send(response.send(coreJs.calculate(request.body)));
 })
 
 app.listen(app.get('port'), function() {
