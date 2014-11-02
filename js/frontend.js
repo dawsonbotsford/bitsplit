82f0eb2cbadc46bb16bf155fc1953654bf5537fb
Brian Newsom <brian.newsom@colorado.edu>
2014-11-02 01:33:09 -0800

    fix post

--- js/frontend.js
+++ js/frontend.js
@@ -1,16 +1,16 @@
-function encode(url, doc) 
+function encode(url, doc)
 {
     window.location.href = url + "?" + doc.amount_field.value;
 }
-function new_page(url) 
+function new_page(url)
 {
     window.location.href = url;
 }
 function build_owe_json(objects){
 
-   $.get("http://localhost:5000/test", function (data) {
+   $.get("./test", function (data) {
       alert(data);
-   }); 
+   });
 
  var jsonret = "{";
   $('#friendlist .btn-info').each(function() {
